4c734df369d93003141301eaacc5200e554c50ed
Rodrigo-Severino-Marques <78482335+Rodrigo-Severino-Marques@users.noreply.github.com>
2022-10-26 18:52:51 +0100

    Add files via upload

--- src/lib/mirrorNode/fetchRemaingSupply.js
+++ src/lib/mirrorNode/fetchRemaingSupply.js
@@ -1,5 +1,5 @@
 // fetch remaining supply data
-const token = '48163190'
+const token = '48738521'
 const treasury = '48509709'
 export default async () => {
     const res = await fetch(`https://testnet.mirrornode.hedera.com/api/v1/accounts/${treasury}/nfts?token.id=${token}`)
