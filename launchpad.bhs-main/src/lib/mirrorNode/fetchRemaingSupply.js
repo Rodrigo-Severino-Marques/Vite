d218b3fe026aa281134fe1bf904f921d30cc2a21
Rodrigo-Severino-Marques <78482335+Rodrigo-Severino-Marques@users.noreply.github.com>
2022-10-26 18:49:40 +0100

    Add files via upload

---
+++ launchpad.bhs-main/src/lib/mirrorNode/fetchRemaingSupply.js
@@ -0,0 +1,9 @@
+// fetch remaining supply data
+const token = '48738521'
+const treasury = '48509709'
+export default async () => {
+    const res = await fetch(`https://testnet.mirrornode.hedera.com/api/v1/accounts/${treasury}/nfts?token.id=${token}`)
+    const data = await res.json()
+    const treasuryBalance = data.nfts.length
+    return treasuryBalance
+}
